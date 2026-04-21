0a1eb2a8a6d993e930f9a66e42a87dda8518298c
alsahlij <J.alsahli@lancaster.ac.uk>
2026-04-21 22:37:59 +0500

    Initial project structure with resources folder and Main entry point

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main entry point for Snow Problem game.
+ * This class launches the application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Snow Problem - Starting...");
+    }
+}
